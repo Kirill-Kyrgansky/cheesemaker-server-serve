b5767c09a02ca75bd00786ed38d2409dff6502c6
Kirill-Kyrgansky <90561275+Kirill-Kyrgansky@users.noreply.github.com>
2022-08-30 10:25:28 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ const {
 } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  configureWebpack: {
+    devServer: {
+      headers: { "Access-Control-Allow-Origin": "*" },
+      proxy: 'http://172.16.0.179/api/'
+    }
+  },
   transpileDependencies: true,
   css: {
     loaderOptions: {
