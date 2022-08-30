5969187cd25c8c4f8cfc66927f7f4d3167bcc23b
Kirill-Kyrgansky <90561275+Kirill-Kyrgansky@users.noreply.github.com>
2022-08-30 11:06:13 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
     devServer: {
       headers: { "Access-Control-Allow-Origin": "*" },
       proxy: 'http://172.16.0.179/api/',
-      port: 8080
     }
   },
   transpileDependencies: true,
